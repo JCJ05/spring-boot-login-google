60ad669a66b33168be69023db76d070894aed005
JCJ05 <julitoaguerorengifo@hotmail.com>
2022-10-03 15:27:05 -0500

    Esquema del Proyecto

---
+++ spring-web-login/src/main/java/com/login/provedores/springweblogin/SpringWebLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.provedores.springweblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebLoginApplication.class, args);
+	}
+
+}
---
+++ spring-web-login/src/test/java/com/login/provedores/springweblogin/SpringWebLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.provedores.springweblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
